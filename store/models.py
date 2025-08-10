b7f99b93381dc8a5eeebfdf187a9090164c70404
Percy Bellido <pbellido0401@gmail.com>
2025-08-09 20:38:39 -0500

    ultimos cambios

--- store/models.py
+++ store/models.py
@@ -61,6 +61,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering=['category']
 
 class Order(models.Model):
     product=models.ForeignKey(Product, on_delete=models.CASCADE)
